98d4f483e61c1a4e41c50a8ad8c3dfefd9cf7ab8
anneraev <47757104+anneraev@users.noreply.github.com>
2019-05-03 15:35:24 -0500

    location-specifc path specification for challenge buttons

--- src/components/challenges/ChallengesList.js
+++ src/components/challenges/ChallengesList.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 
 export default class ChallengesList extends Component {
-    render(){
+    //clicking button will redirect to different page based on the URL of the page the buttons are displayed on. In "Home", displays read-only messages. From "profile", messages and issues are editable. Looks for string in pathname.
+    challengeListButtonHandler = (issue) => {
+        console.log(this.props.history)
+        if (this.props.history.location.pathname === "/profile") {
+            return (<button key={issue.id} onClick={() => this.props.history.push(`/challenge-edit/${issue.id}`)}>
+            {issue.content}
+        </button>)
+        } else {
+        return (<button key={issue.id} onClick={() => this.props.history.push(`/challenge-messages/${issue.id}`)}>
+            {issue.content}
+        </button>)
+        }
+    }
+
+    render() {
         //renders section with .map function that creates an array of items with JSX wrappers from the items inside the array of objects specified.
-        return(
+        return (
             <React.Fragment>
                 <section>
                     {
-                        this.props.issues.map(issue =>
-                            <button key={issue.id} onClick={() => this.props.history.push(`/challenge-messages/${issue.id}`)}>
-                                {issue.content}
-                            </button>
-                        )
+                        this.props.issues.map(issue => this.challengeListButtonHandler(issue))
                     }
                 </section>
             </React.Fragment>
