791678a8afe53c349fd12a10492bdb6b2f1fe976
anneraev <47757104+anneraev@users.noreply.github.com>
2019-05-05 16:02:22 -0500

    created message edit window

--- src/components/messages/MessageEditList.js
+++ src/components/messages/MessageEditList.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import MessagesList from "./MessagesList";
 
+//create new message, open edit window. Track message change and add it to state. Update message state when clickign "add/edit" The window needs a reference to the id of the message.
+
+
 export default class MessageEditList extends Component {
+    createMessageEditWindow = () => {
+        return(
+            <React.Fragment>
+            <h2>Write messages to help yourself navigate your challenges</h2>
+            <textarea></textarea>
+            </React.Fragment>
+        )
+    }
+
     render(){
         console.log(this.props)
         return(
             <React.Fragment>
                 <section>
+                    {this.createMessageEditWindow()}
                     <MessagesList {...this.props}/>
                 </section>
             </React.Fragment>
--- src/components/messages/MessagesList.js
+++ src/components/messages/MessagesList.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 
-let returnedItems = false;
-
 export default class MessagesList extends Component {
 showApplicableMessages = (message) => {
         //uses a conditional to see if the message's issueId matches the issueId passed through URL parameter when this page was routed to. The URL parameter is the same as the ID of the issue topic that was clicked on. If it does match, this returns the content of that message as a result, and since it was called within the render function, that message's content will be rendered. Another conditional determines that the list was called from the edit function, and whether or not there are any messages to edit. If not, it renders a button for creating a new message instead.
         if (message.issueId === Number(this.props.match.params.issueId)){
-            returnedItems = true
             return(
                 <li key={message.id}>
                 {message.content}
@@ -15,7 +12,7 @@ showApplicableMessages = (message) => {
         }
     }
 
-    //new message item in list only in create/edit pages.
+    //new message item in list when clicking on a message to edit/create.
     addNewMessage = () => {
         if (this.props.location.pathname === "/profile/challenges/new") {
         return (
