bea7521b487ff468a555896027c85572a4b2c627
anneraev <47757104+anneraev@users.noreply.github.com>
2019-05-05 23:14:44 -0500

    edit region for issue title now displays the current state of issue

--- src/components/challenges/ChallengesEdit.js
+++ src/components/challenges/ChallengesEdit.js
@@ -10,7 +10,7 @@ import MessageEditList from "../messages/MessageEditList";
 export default class ChallengeEdit extends Component {
     //issue object state
     state = {
-        content: "",
+        content: this.props.issues[this.props.match.params.issueId - 1].content,
         userId: 1,
         active: true,
         id: this.props.match.params.issueId
@@ -26,10 +26,11 @@ export default class ChallengeEdit extends Component {
     //on Change takes a reference to a function that runs when something changes in the input field. Ref takes an anonymous callback function, in this case it creates a key/value pair belonging to this object, and passes a reference to that function to set the value to a reference to the element.
     render() {
         console.log(this.props)
+        console.log("issues", this.props.issues);
         return (
             <React.Fragment>
                 <section className="form-group">
-                    <input type="text" required className="form-control" onChange={this.handleInput} id="content" placeholder="What would you like help with?" ref={(element) => this.challengeInput = element}>
+                    <input type="text" required className="form-control" onChange={this.handleInput} id="content" placeholder="What would you like help with?" ref={(element) => this.challengeInput = element} value={this.state.content}>
                     </input>
                 </section>
                 <MessageEditList {...this.props} />
