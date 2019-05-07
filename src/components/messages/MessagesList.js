5ddbc882f2789f9157084f3c7cb82046e7de6f3a
anneraev <47757104+anneraev@users.noreply.github.com>
2019-05-07 09:38:11 -0500

    added active/inactive buttons to messages

--- src/components/messages/MessagesList.js
+++ src/components/messages/MessagesList.js
@@ -9,7 +9,29 @@ export default class MessagesList extends Component {
     }
 
     handleDelete = (event) => {
-        this.props.deleteMessage(event.target.id).then(() => this.updateData());
+        this.props.deleteMessage(event.target.id).then(() => this.props.updateData());
+    }
+
+    //displays a button that, when clicked, toggles active/inactive flag for the message in the data. The appearance of the button changes based on the status of the message.
+    renderActiveButton = (message) => {
+        if (message.active === true) {
+            return(
+                <React.Fragment>
+                    <button>
+                        Active
+                    </button>
+                </React.Fragment>
+            )
+        } else
+        {
+            return(
+                <React.Fragment>
+                    <button>
+                        Inactive
+                    </button>
+                </React.Fragment>
+            )
+        }
     }
 
     renderDeleteButton = (message) => {
@@ -25,13 +47,14 @@ export default class MessagesList extends Component {
     }
 
     showApplicableMessages = (message) => {
-        //uses a conditional to see if the message's issueId matches the issueId passed through URL parameter when this page was routed to. The URL parameter is the same as the ID of the issue topic that was clicked on. If it does match, this returns the content of that message as a result, creates an elemement containing it, and passes that to the render function.
+        //uses a conditional to see if the message's issueId matches the issueId passed through URL parameter when this page was routed to. The URL parameter is the same as the ID of the issue topic that was clicked on. If it does match, this returns the content of that message as a result, creates an elemement containing it, and passes that to the render function. If called from "/home", filters out inactive messages.
         if (this.props.location.pathname.includes("profile/challenges/")) {
             if (message.issueId === Number(this.props.match.params.issueId)) {
                 return (
                     <li key={message.id} id={message.id}>
                         <span key={message.id} id={message.id} onClick={this.handleMessageClick}>{message.content}</span>
                         {this.renderDeleteButton(message)}
+                        {this.renderActiveButton(message)}
                     </li>
                 )
             }
